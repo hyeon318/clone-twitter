62a465ec5ebd5a78285db8dc0a3e728add0afefb
spidey <kyurin123@gmail.com>
2023-02-24 19:06:03 +0900

    file upload

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,16 @@
-import { dbService } from 'fbase';
+import { upload } from '@testing-library/user-event/dist/upload';
+import Nweet from 'components/Nweet';
+import { dbService, storageService } from 'fbase';
 import { addDoc, collection, getDocs, onSnapshot,  orderBy,  query } from 'firebase/firestore';
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
 import React, { useEffect, useState } from "react";
+import { v4 } from 'uuid';
 
 const Home = ({userObj}) => {
-  const [nweet, setNweet] = useState("");
-  const [nweets, setNweets] = useState([]);
+  const [nweet, setNweet] = useState("");           // nweet 1개 info
+  const [nweets, setNweets] = useState([]);         // nweets list
+  const [attachment, setAttachment] = useState("")  // 첨부파일 
 
-  
 // export declare function onSnapshot<T>(query: Query<T>, observer: {
 //     next?: (snapshot: QuerySnapshot<T>) => void;
 //     error?: (error: FirestoreError) => void;
@@ -32,7 +36,7 @@ const Home = ({userObj}) => {
         }
       });
       setNweets(nweetArr);
-      console.log("Current Nweets in CA ",nweetArr);
+      // console.log("Current Nweets in CA ",nwezetArr);
     })
   }
 
@@ -53,12 +57,29 @@ const Home = ({userObj}) => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    const docRef = await addDoc(collection(dbService, "nweets"), {
+    let attachmentUrl = "";
+    if(attachment !== ""){
+      const attachmentRef = ref(storageService, `${userObj.uid}/${v4()}`);
+      const response = await uploadString(attachmentRef, attachment, "data_url");
+      attachmentUrl = await getDownloadURL(response.ref);
+    
+    }
+    setNweet("");
+    setAttachment("");
+    const nweetObject = {
       text : nweet
       , createdAt : Date.now()
       , creatorId : userObj.uid
-    })
-    setNweet("");
+      , attachmentUrl 
+    }
+    await addDoc(collection(dbService, "nweets"), nweetObject)
+
+    // const docRef = await addDoc(collection(dbService, "nweets"), {
+    //   text : nweet
+    //   , createdAt : Date.now()
+    //   , creatorId : userObj.uid
+    // })
+    // setNweet("");
   }
   
   const onChange = (event) => {
@@ -66,20 +87,42 @@ const Home = ({userObj}) => {
     setNweet(value);
   }
 
-  // console.log(nweets)
+  const onFileChange = (event) => {
+    const {target:{files}} = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = (finishedEvent) => {
+      const {currentTarget: {result}} = finishedEvent
+      setAttachment(result);
+    }
+
+    if(theFile){
+      reader.readAsDataURL(theFile);
+    }
+
+    console.log(event.target.files)
+  } 
+
+  const onClearAttachment = () => {
+    setAttachment(""); 
+  }
 
   return (
   <div>
     <form onSubmit={onSubmit}>
       <input type="text" value={nweet} onChange={onChange} placeholder="What's Your Mind?" maxLength={120} />
+      <input type="file" accept='image/*' onChange={onFileChange} />
+        {attachment && <div>
+                        <img src={attachment} width="50px" height="50px"/>
+                        <button onClick={onClearAttachment}>Clear</button>
+                      </div>}
       <input type="submit" value="Nweet"/>
     </form>
     <div>
       {
         nweets.map(nweet => {
-          return (<div key={nweet.id}>
-            <h4>{nweet.text}</h4>
-          </div>)
+          return (<Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid}/>)
         })
       }
     </div>

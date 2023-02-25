62b7b49df816826d33b36c201b720f0727fd6706
spidey <kyurin123@gmail.com>
2023-02-26 00:56:05 +0900

    git deploying

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App';
 import firebase from './fbase'
 
@@ -7,7 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // strictmode는 side effect를 줄이기 위해 일부러 두번씩 실행시킨다고 하네요. 그래서 dev환경에서만 두번씩 호출되고 production에서는 무시된다고 합니다.
   // <React.StrictMode>
-    <App />
+  <HashRouter base="/">
+    <App/>
+  </HashRouter>
   // </React.StrictMode>
 );
 

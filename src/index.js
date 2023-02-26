ea02ec371827681aa22d1bf354831a7fb7077894
spidey <kyurin123@gmail.com>
2023-02-26 21:16:47 +0900

    index 수정

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // strictmode는 side effect를 줄이기 위해 일부러 두번씩 실행시킨다고 하네요. 그래서 dev환경에서만 두번씩 호출되고 production에서는 무시된다고 합니다.
   // <React.StrictMode>
-  <HashRouter base="/">
     <App/>
-  </HashRouter>
   // </React.StrictMode>
 );
 
